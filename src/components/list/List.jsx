28960089d9f502c59d0dd99f63d5e789003b15e5
Anna Golmakova <ann.golmakova@gmail.com>
2024-07-26 16:21:08 +0100

    Сумма рассчитывается

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -2,7 +2,9 @@ import "./List.css";
 
 import Card from "../card/Card.jsx";
 
-function List({ items, total }) {
+function List({ items }) {
+  const total = items.reduce((sum, item) => sum + item.price, 0);
+
   return (
     <main className="list">
       {items.map((item) => (
